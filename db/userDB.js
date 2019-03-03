e2d4ca6ac6feb4b56d401199ec3116cd743f6ea0
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-03-03 20:56:03 +0100

    create the signup route #164356725

---
+++ db/userDB.js
@@ -0,0 +1,12 @@
+const user = [
+  {
+    id: 1,
+    email: 'Dolapo@epicmail.com',
+    firstName: 'Dolapo',
+    lastName: 'Adeleye',
+    password: 'dolapo',
+    isAdmin: 'true',
+  },
+];
+
+export default user;
